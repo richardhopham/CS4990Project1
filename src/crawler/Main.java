59f01fa3740f952336150f29a797b5de5115f56a
Richard <richardhopham@gmail.com>
2019-03-13 16:31:25 -0700

    Debug

--- src/crawler/Main.java
+++ src/crawler/Main.java
@@ -8,11 +8,11 @@ public class Main {
 		Spider spider = new Spider();
 		spider.search("http://www.cpp.edu");
 		
-		spider = new Spider();
-		spider.search("http://www.reddit.com");
-		
-		spider = new Spider();
-		spider.search("http://en.wikipedia.org/wiki/Dog");
+//		spider = new Spider();
+//		spider.search("http://www.reddit.com");
+//		
+//		spider = new Spider();
+//		spider.search("http://en.wikipedia.org/wiki/Dog");
 	}
 
 }
--- src/crawler/Spider.java
+++ src/crawler/Spider.java
@@ -2,7 +2,6 @@ package crawler;
 
 import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 
